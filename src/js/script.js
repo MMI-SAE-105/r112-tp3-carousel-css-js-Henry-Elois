2a73586d8bee2ee882adf9b716804b2a1f17e196
Henry-Elois <elois.henry@edu.univ-fcomte.fr>
2024-12-02 13:45:13 +0100

    défilement en fonction de la largeur d'une image

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,20 @@
-// *** CAROUSEL ***
-// TODO//
+const carousel = (".carouselcontainer")
+const prevButton = (".carouselbuttonprev");
+const nextButton = (".carouselbuttonnext");
+
+if (carousel) {
+  
+  
+  prevButton.addEventListener("click", () => {
+    carousel.scrollBy({left:scrollAmount, behavior: "smooth"}); 
+  });
+  
+  nextButton.addEventListener("click", () => {
+    carousel.scrollBy({left:scrollAmount, behavior: "smooth"});
+  });
+}
+
+document.querySelector(".carouselcontainer")
+const scrollAmount = firstItem.clientWidth;
 
 
